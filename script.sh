7d84e12063f6d43f3c9e8342ad572944d4735eda
HaloDragon7410 <68484158+HaloDragon7410@users.noreply.github.com>
2021-10-20 14:28:43 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,26 @@
+#!/bin/sh
+apt update && apt upgrade
+apt install dropbear wget
 
+# Dropbear
+# Banner location is /etc/banner
+echo "Configuring Dropbear"
+touch /etc/banner
+wget https://raw.githubusercontent.com/HaloDragon7410/ws-gaming-script/master/dropbear -O /etc/default/dropbear
+systemctl start dropbear
+
+# Websocket
+echo "Configuring Websocket"
+wget https://raw.githubusercontent.com/HaloDragon7410/ws-gaming-script/master/ws-dropbear -O /usr/local/bin/ws-dropbear
+wget https://raw.githubusercontent.com/HaloDragon7410/ws-gaming-script/master/ws.service -O /etc/systemd/system/ws.service
+sudo chmod +x /usr/local/bin/ws-dropbear
+systemctl start ws.service
+
+# Gaming (BadVPN-UDPGW)
+echo "Configuring BadVPN-UDPGW"
+wget https://raw.githubusercontent.com/HaloDragon7410/ws-gaming-script/master/badvpn.service -O /etc/systemd/system/badvpn.service
+wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/daybreakersx/premscript/master/badvpn-udpgw64"
+sudo chmod +x /usr/bin/badvpn-udpgw
+systemctl start badvpn.service
+
+echo "Done"
