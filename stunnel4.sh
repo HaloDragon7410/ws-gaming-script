31077b674a046429691d94ea1ceee371b50e0504
root <root@localhost.localdomain>
2021-10-31 20:31:55 +0000

    Forgot cert

--- stunnel4.sh
+++ stunnel4.sh
@@ -1,7 +1,9 @@
-apt install stunnel4
+apt install stunnel4 ca-certificates
 
 wget https://raw.githubusercontent.com/HaloDragon7410/ws-gaming-script/master/stunnel4 -O /etc/stunnel/stunnel4
 wget https://raw.githubusercontent.com/HaloDragon7410/ws-gaming-script/master/stunnel.conf -O /etc/stunnel/stunnel.conf
+
+openssl req -new -out /etc/stunnel/vpn.pem -keyout /etc/stunnel/vpn.pem -nodes -x509 -days 365
 
 systemctl enable stunnel4
 systemctl start stunnel4
